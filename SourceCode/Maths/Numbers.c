586fc78c733a0d41bb32071ca9f1501a7ec989d1
atifashhar <atifashhar@gmail.com>
2020-04-30 03:40:30 -0400

    Final

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -2,12 +2,11 @@
 int main(){
 printf(1+2);
 }
-<<<<<<< HEAD
+
 int add(int a, int b){
 	return a+b;}
 
-=======
 int mult(int a,int b){
   return a*b;
 }
->>>>>>> 75e8ad0a2553a0c6cb1a75610a8edfd49f7b2bce
+
